e6d5dec5e5ca0f72dd85ea4246168b6ecfd1e811
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2020-10-08 09:58:52 +0300

    Lab 2

---
+++ src/main/scala/Lab2/Ex1.scala
@@ -0,0 +1,7 @@
+package Lab2
+
+object Ex1{
+    def main(args: Array[String]) = {
+        println("Hello World")
+    }
+}
---
+++ src/main/scala/Lab2/Persoana.scala
@@ -0,0 +1,26 @@
+package Lab2
+
+import java.util.ArrayList
+import com.cra.figaro.language._
+import com.cra.figaro.algorithm.sampling
+
+abstract class Persoana{
+    def main(args: )
+}
+
+class Student extends Persoana( var nume: String, var prenume: String,var an: Int, val materii: Array[String, Integer]){
+    def main(args: Array[String, Integer]){
+        setNota(materie: String, nota: Integer)
+        getNota(materie:String)
+        addMaterie(materie: String, nota: Integer)
+    }
+    
+}
+
+class Profesor extends Persoana(var nume: String, var prenume: String, var materie: String){
+
+}
+
+class Materie(var numeMaterie: Array[String]){
+    val numeMaterie = Array("PMP","Mate","Logica","OOP")
+}
