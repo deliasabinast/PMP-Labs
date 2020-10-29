3866008178db56c0c6e1f9f18a3fb656ea29c001
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2020-10-29 09:53:28 +0200

    Lab 5 - tried to add 2 more cards

--- src/main/scala/Lab5/Ex1.scala
+++ src/main/scala/Lab5/Ex1.scala
@@ -1,15 +1,72 @@
 package Lab5
 
-import com.cra.figaro.language._
-import com.cra.figaro.algorithm.sampling._
-
+import com.cra.figaro.library.atomic.discrete
+import com.cra.figaro.language.Chain
+import com.cra.figaro.library.compound.{RichCPD, OneOf, *}
+import com.cra.figaro.language.{Flip, Constant, Apply}
+import com.cra.figaro.algorithm.factored.VariableElimination
 object Ex1 {
-	def main(args: Array[String]) {
-		val test = Constant("Test")
+def main(args: Array[String]) {
 
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
-	}
+// To keep the code simple, I just make the cards an integer
+val cards = List(5, 4, 3, 2, 1)
+// The discrete uniform distribution chooses uniformly from a fixed
+// set of possibilities
+
+val player1Card = discrete.Uniform(cards:_*)
+val player2Card = Chain(player1Card, (card: Int) => discrete.Uniform(cards.filter(_ != card):_*))
+// Player 2 can get any card except the first player’s card
+
+//tried to add 1 more card for each player
+val player1Card2 = Chain(player1Card, player2Card (card: Int) => discrete.Uniform(cards.filter(_ != card):_*))
+val player2Card2 = Chain(player1Card, player2Card, player1Card2 (card: Int) => discrete.Uniform(cards.filter(_ != card):_*))
+
+val player1Bet1 = RichCPD(player1Card, OneOf(5, 4, 3) -> Flip(0.9),* -> Flip(0.4))
+// Player 1 is more likely to bet with a higher card,
+// but will sometimes bet with a lower card to bluff
+ // ×××Change this for part (c)×××
+val player2Bet =RichCPD(player2Card, player1Bet1,(OneOf(5, 4), *) -> Flip(0.9),(*, OneOf(false)) -> Flip(0.5),(*, *) -> Flip(0.1))
+val player1Bet2 =Apply(player1Card, player1Bet1, player2Bet,(card: Int, bet11: Boolean, bet2: Boolean) =>!bet11 && bet2 && (card == 5 || card == 4))
+// Player 1’s second bet is only relevant if she passed the
+// first time and player 2 bet
+
+// This element represents the gain to player 1 from the game. I have
+// made it an Element[Double] so I can query its mean.
+val player1Gain =Apply(player1Card, player2Card, player1Bet1, player2Bet, player1Bet2,(card1: Int, card2: Int, bet11: Boolean,bet2: Boolean, bet12: Boolean) =>
+if (!bet11 && !bet2) 0.0
+else if (bet11 && !bet2) 1.0
+else if (!bet11 && bet2 && !bet12) -1.0
+else if (card1 > card2) 2.0
+else -2.0)
+
+
+player1Card.observe(4)
+player1Bet1.observe(true)
+val alg1 = VariableElimination(player1Gain)
+alg1.start()
+alg1.stop()
+println("Expected gain for betting:" + alg1.mean(player1Gain))
+player1Bet1.observe(false)
+val alg2 = VariableElimination(player1Gain)
+alg2.start()
+alg2.stop()
+println("Expected gain for passing:" + alg2.mean(player1Gain))
+player1Card.unobserve()
+player1Bet1.unobserve()
+
+
+
+player2Card.observe(3)
+player1Bet1.observe(true)
+player2Bet.observe(true)
+val alg3 = VariableElimination(player1Gain)
+alg3.start()
+alg3.stop()
+println("Expected gain for betting:" + alg3.mean(player1Gain))
+player2Bet.observe(false)
+val alg4 = VariableElimination(player1Gain)
+alg4.start()
+alg4.stop()
+println("Expected gain for passing:" + alg4.mean(player1Gain))
+}
 }
