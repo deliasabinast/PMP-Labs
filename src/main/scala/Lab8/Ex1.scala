1183f5ea4c8053bce0a010d2a94d6ddb9c50c1d6
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2020-11-24 09:50:42 +0200

    Update Ex1.scala

--- src/main/scala/Lab8/Ex1.scala
+++ src/main/scala/Lab8/Ex1.scala
@@ -9,7 +9,7 @@ object Alarma{
 	Universe.createNew()
  	private val alarmNotRings = Flip(0.1) //prob. ca alarma sa nu sune
 	private val busLate = Flip(0.2) // prob. sa intarzie autobuzul
-	private val sleepsAgain = CPD(alarm,true->Flip(0.01)) //prob. sa adoarma la loc, chiar daca a sunat alarma
+	private val sleepsAgain = CPD(alarm,false->Flip(0.01)) //prob. sa adoarma la loc, chiar daca a sunat alarma
 	private val doesntWakeup = CPD(alarmNotRings,sleepsAgain, (true,true)-> Flip(0.2)) //prob. sa nu se trezeasca, fie pt. ca nu a pus alarma, fie pt ca a adormit la loc
 	// prob. sa intarzie:
 	private val isLate=CPD(doesntWakeup,busLate,(false, false)-> Flip(0.1), (false, true)->Flip(0.8), (true, false)-> Flip(0.7), (false, false)-> Flip(0.9)) 
