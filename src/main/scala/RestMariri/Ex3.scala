af992b2eb77893343ef91d4f188b078476745dd3
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2021-02-08 15:04:01 +0200

    updated ex3

--- src/main/scala/RestMariri/Ex3.scala
+++ src/main/scala/RestMariri/Ex3.scala
@@ -14,27 +14,36 @@ object RestMariri {
   def main(args: Array[String])
   {
    
-
+    //A:
     // clasa abstracta pt modelul Markov
     abstract class State {
-      var starePacient = Array[Element[Boolean]] = Array.fill(chapters)(Constant('buna))
+      var starePacient = Array[Element[Boolean]] = Array.fill(iterations)(Constant('buna))
     }
-
+    //B:
     // clasa starii initiale
     class initialState() extends State {
       // setam probabilitatile conform tabelului:
+      //starea initiala este A, buna
+      //iar pt fiecare dintre celelalte stari selectam valoarea corespunzatoare liniei si coloanei de pe aceeasi stare
       stare(0) = Select(0.721 -> 'buna, 0.581 -> 'nuPreaBuna, 0.75 -> 'bolnav, 1.0 -> 'decedat)
     }
-
+    //C:
     //clasa pentru starea urmatoare starii curente
     class NextState(current: State, iteration: Int) extends State {
       // de aici luam valorile fiecarei stari:
-      stare(iteration) = CPD(stare(chapter - 1),
+      current(iteration) = CPD(current(iteration - 1),
         'buna -> Select(0.721 -> 'buna, 0.202 -> 'nuPreaBuna, 0.067 -> 'bolnav, 0.1 -> 'decedat),
         'nuPreaBuna -> Select(0 -> 'buna, 0.581 -> 'nuPreaBuna, 0.407 -> 'bolnav, 0.012 ->'decedat),
         'bolnav -> Select(0 -> 'buna, 0 -> 'nuPreaBuna, 0.75 -> 'bolnav, 0.25 -> 'decedat),
         'decedat -> Select(0 -> 'buna, 0 -> 'nuPreaBuna, 0 -> 'bolnav, 1.0 -> 'decedat))
     }
+
+    //D:
+    //lista cu 10 stari
+
+    //F:
+    starePacient(9).observe('nuPreaBuna)
+    println(VariableElimination.probability(starePacient(10), 'nuPreaBuna))
 
   }
 }
