4cca4a5aef5a01e0ffbd1b09db85f68220f6686e
deliasabinast <59909663+deliasabinast@users.noreply.github.com>
2021-02-08 15:32:45 +0200

    update

--- src/main/scala/RestMariri/Ex1.scala
+++ src/main/scala/RestMariri/Ex1.scala
@@ -33,8 +33,8 @@ object Ex1{
       else "normala" //altfel.. 
   }
     //B:
-    snowyDaysInAWeek.setCondition((i: Int) => i<=5)
-    println(VariableElimination.probability(week, "normala"))
+    snowyDaysInAWeek.setCondition((i: Int) => i<=5 && i>2) //conditia ca sapt sa fie normala
+    println(VariableElimination.probability(week, "normala")) //probabilitatea ca sapt sa fie normala
 
 
 
